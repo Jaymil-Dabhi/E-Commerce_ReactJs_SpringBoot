93fa25bf34dff0ccbe76570d34d158d82d540aad
jaymildabhi13 <jaymildabhi49@gmail.com>
2024-02-13 18:32:08 +0530

    frontend part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import HomePage from './customer/pages/HomePage/HomePage';
+import Footer from './customer/components/Footer/Footer';
+import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navigation/>
+       <div>
+         {/* <HomePage/> */}
+         {/* <Product/> */}
+         {/* <ProductDetails/> */}
+         <Cart/>
+       </div>
+       <Footer/>
     </div>
   );
 }
